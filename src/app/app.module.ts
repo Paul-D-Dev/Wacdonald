32220ddf6dff046172f74e46015de1f66bed2c49
Paul DAO <dao.van.paul@gmail.com>
2019-11-03 19:10:58 +0100

    add create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardMenuComponent } from './components/card-menu/card-menu.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CardMenuComponent,
+    CardProductComponent
   ],
   imports: [
     BrowserModule,
