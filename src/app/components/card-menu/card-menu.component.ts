8e289b041dcb1ddba89388fb5c4fbdaf70a2152c
Paul DAO <dao.van.paul@gmail.com>
2019-11-03 23:06:47 +0100

    add main elm and sidebar

--- src/app/components/card-menu/card-menu.component.ts
+++ src/app/components/card-menu/card-menu.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardMenuComponent implements OnInit {
 
+  cardMenu = [
+    {
+      image : '',
+      title : 'Combo Meal',
+      price : 3,
+    },
+    {
+      image : '',
+      title : 'Burgers & Sandwiches',
+      price : 3,
+    },
+    {
+      image : '',
+      title : 'Happy Meal',
+      price : 3,
+    },
+    {
+      image : '',
+      title : 'Beverages',
+      price : 3,
+    },
+    {
+      image : '',
+      title : 'Chicken',
+      price : 3,
+    },
+    {
+      image : '',
+      title : 'Snack & Sides',
+      price : 3,
+    },
+
+  ]
   constructor() { }
 
   ngOnInit() {
