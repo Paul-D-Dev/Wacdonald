6b11b71ff806903590b7c56bd76bd461ed85c6d1
Paul DAO <dao.van.paul@gmail.com>
2019-11-04 09:24:24 +0100

    import menu.json

--- src/app/components/card-menu/card-menu.component.ts
+++ src/app/components/card-menu/card-menu.component.ts
@@ -9,37 +9,38 @@ export class CardMenuComponent implements OnInit {
 
   cardMenu = [
     {
-      image : '',
+      image : '../../../assets/icons/burger.svg',
       title : 'Combo Meal',
       price : 3,
     },
     {
-      image : '',
+      image : '../../../assets/icons/burger.svg',
       title : 'Burgers & Sandwiches',
       price : 3,
     },
     {
-      image : '',
+      image : '../../../assets/icons/birthday-cake.svg',
       title : 'Happy Meal',
       price : 3,
     },
     {
-      image : '',
+      image : '../../../assets/icons/burger.svg',
       title : 'Beverages',
       price : 3,
     },
     {
-      image : '',
+      image : '../../../assets/icons/chicken-leg.svg',
       title : 'Chicken',
       price : 3,
     },
     {
-      image : '',
+      image : '../../../assets/icons/french-fries.svg',
       title : 'Snack & Sides',
       price : 3,
     },
 
-  ]
+  ];
+
   constructor() { }
 
   ngOnInit() {
