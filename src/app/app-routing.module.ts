afa51c647b176cdf77f0b1b50df9a382cd828297
Paul DAO <dao.van.paul@gmail.com>
2020-01-21 13:39:34 +0100

    call routes admin secure by guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { AuthGuard } from './core/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
